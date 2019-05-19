c0bdccae174a83ed054f919a6f2b190dd48baad2
tyc105u <45002199+tyc105u@users.noreply.github.com>
2019-05-19 19:17:35 +0800

    Add files via upload

--- menu-screen.js
+++ menu-screen.js
@@ -3,8 +3,38 @@
 //
 // See HW4 writeup for more hints and details.
 class MenuScreen {
-  constructor() {
-    // TODO(you): Implement the constructor and add fields as necessary.
+  constructor(containerElement) {
+    this.containerElement = containerElement;
+    var Theme = ['candy', 'charlie brown', 'computers', 'dance', 'donuts', 'hello kitty', 'flowers', 'nature', 'turtles', 'space'];
+
+    function onJsonReady(json) {
+      let tmp = Math.floor(Math.random() * Math.floor(Theme.length));
+      document.querySelector('#query-input').setAttribute("value", Theme[tmp]);
+
+      var node = document.querySelector('#song-selector');
+
+      var track = new Object();
+      for (let i in json){
+        //console.log(i);
+        track[i] = json[i].songUrl;
+        node.append(new Option(json[i].title, track[i]));
+      }
+    }
+
+    fetch('https://fullstackccu.github.io/homeworks/hw4/songs.json')
+      .then(function onResponse(response) {
+        return response.json();
+      })
+      .then(onJsonReady);
   }
-  // TODO(you): Add methods as necessary.
+
+  show() {
+    this.containerElement.classList.remove('inactive');
+  }
+
+  hide() {
+    this.containerElement.classList.add('inactive');
+  }
+
 }
+
